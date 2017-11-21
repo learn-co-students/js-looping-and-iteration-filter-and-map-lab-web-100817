7c00d1619ede6998cd1fa3f02e9b9c7adb225d26
Abe Rudy <aberudy@gmail.com>
2017-11-21 00:15:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here:
+function driversWithRevenueOver (collection, revenue) {
+  return collection.filter(function (employee) {
+    return employee.revenue >= revenue
+  })
+}
+
+function driverNamesWithRevenueOver(collection, revenue) {
+  let newCollection
+  newCollection = driversWithRevenueOver(collection, revenue)
+  return newCollection.map(function (highRollers) {
+    return highRollers.name
+  })
+}
+
+function exactMatch (collection, attribute) {
+  let attr = Object.keys(attribute)
+  // debugger
+  return collection.filter(function (driverName) {
+    return driverName[attr] === attribute[attr]
+  })
+}
+
+
+function exactMatchToList (collection, attribute) {
+  let matches = exactMatch(collection, attribute)
+  return matches.map(function (driver) {
+    return driver.name
+  })
+
+}
